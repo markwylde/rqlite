184a0e98c624f9f199fe374f6858fab16b8cb69b
Mark Wylde <me@markwylde.com>
2020-06-04 00:24:04 +1000

    Connect should follow redirects

--- connect.js
+++ connect.js
@@ -1,18 +1,21 @@
 const callarest = require('callarest/json');
+const { http, https } = require('follow-redirects');
 
 function connect (url, callback) {
   callarest({
-    url: `${url}/db/query?q=SELECT%20date(%27now%27)&pretty&timings`
+    url: `${url}/db/query?q=SELECT%20date(%27now%27)&pretty&timings`,
+    httpAgent: http,
+    httpsAgent: https
   }, function (error, rest) {
     if (error) {
       return callback(error);
     }
 
-    if (rest.body.results && rest.body.time) {
+    if (rest.body && rest.body.results && rest.body.time) {
       return callback(null, { url });
     }
 
-    console.log(rest.body);
+    console.log('Body returned was: ', rest.body);
     return callback(new Error('url does not look like an rqlite database'));
   });
 }
